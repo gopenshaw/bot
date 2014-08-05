69322d44c3337506c8e1294844fa362a257595c5
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-08-05 06:02:14 -0400

    New communication methods for getting a setting a general location. The idea is the hq will set the location and the soldiers will go there.

--- Communication.java
+++ Communication.java
@@ -5,10 +5,12 @@ import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public class Communication {
-	final static int BUILD_PASTR_INDEX = 0;
-	final static int ENEMY_PASTR_COUNT_INDEX = 1;
-	final static int ENEMY_PASTR_LOCATION_DATA_START = 2;
+	final static int ENEMY_PASTR_COUNT_INDEX = 2;
+	final static int ENEMY_PASTR_LOCATION_DATA_START = 3;
 	final static int ENEMY_PASTR_LOCATION_DATA_SIZE = 20;
+	
+	final static int DESTINATION_DATA_START = 0;
+	final static int DESTINATION_DATA_SIZE = 2;
 	
 	protected static void broadcastEnemyPastrLocations(int index, MapLocation location, RobotController rc) 
 			throws GameActionException {
@@ -31,5 +33,18 @@ public class Communication {
 	protected static int getEnemyPastrCount(RobotController rc) throws GameActionException
 	{
 		return rc.readBroadcast(ENEMY_PASTR_COUNT_INDEX);
+	}
+	
+	protected static void broadcastDestination(MapLocation location, RobotController rc) 
+			throws GameActionException {
+		rc.broadcast(DESTINATION_DATA_START, location.x);
+		rc.broadcast(DESTINATION_DATA_START + 1, location.y);
+	}
+	
+	protected static MapLocation getDestination(RobotController rc) 
+			throws GameActionException
+	{
+		return new MapLocation(rc.readBroadcast(DESTINATION_DATA_START),
+				rc.readBroadcast(DESTINATION_DATA_START + 1));
 	}
 }
