b1e3500f1d035bae85a36a51c94942d18f7ac215
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-08-05 18:39:45 -0400

    Adding a todo

--- Communication.java
+++ Communication.java
@@ -12,6 +12,7 @@ public class Communication {
 	final static int DESTINATION_DATA_START = 0;
 	final static int DESTINATION_DATA_SIZE = 2;
 	
+	//--TODO: use one channel to broadcast a location. The value can represent, x + 100y
 	protected static void broadcastEnemyPastrLocations(int index, MapLocation location, RobotController rc) 
 			throws GameActionException {
 		rc.broadcast(ENEMY_PASTR_LOCATION_DATA_START + index * 2, location.x);
