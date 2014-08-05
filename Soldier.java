637e9f52240c60f25c884e0a6ab85df7a17080f9
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-08-05 06:36:55 -0400

    Added one more TODO

--- Soldier.java
+++ Soldier.java
@@ -58,10 +58,15 @@ public class Soldier {
 	//--TODO: This method will cause the robot to go into an infinite loop
 	//if the distance to the destination from the corners of an obstacle are never
 	//shorter than the initial distance when the robot first hit the obstacle.
+	
 	//--TODO: A related problem is that if the destination changes while the robot
 	//if following a wall, it needs to adjust to the new distance somehow
+	
 	//--TODO: A robot needs to leave behind 'waypoints' to indicate that the path
 	//has shortcuts. Other robots will follow these 'waypoints' if they exist
+	
+	//--TODO: Does this method and the 'followWall' method need to return a MovementInfo
+	//object??
 	private static MovementInfo goToDestination(
 			MapLocation destination, RobotController rc, MovementInfo movementStatus) 
 			throws GameActionException 
