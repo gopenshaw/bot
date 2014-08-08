4b36f5d3c6dec70195c0345f465f0de1825d20c2
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-08-08 19:55:54 -0400

    trying out the refactored movement method. Should function exactly the same as the previous movement method.

--- MovementLogic.java
+++ MovementLogic.java
@@ -2,33 +2,84 @@ package bot;
 
 import battlecode.common.*;
 
+//--Throughout this class, if the robot hits a wall, it will turn to
+//the left. In other words, it will keep the wall on its right.
 public class MovementLogic {
 	private MapLocation destination;
-	private MapLocation segmentBegin;
-	private MapLocation segmentEnd;
 	private Direction currentDirection;
 	private boolean followingWall;
+	private int initialDistance;
 	
-	//--We will assume the robot keeps the wall on its left
-	public void moveTowards(MapLocation destination, RobotController rc) 
+	//--TODO: The moveTowards method will cause the robot to go into an infinite loop
+	//if the distance to the destination from the corners of an obstacle are never
+	//shorter than the initial distance when the robot first hit the obstacle.
+	//--TODO: A robot needs to leave behind 'waypoints' to indicate that the path
+	//has shortcuts. Other robots will follow these 'waypoints' if they exist
+	public void moveToward(MapLocation destination, RobotController rc) 
 			throws GameActionException
 	{
 		//--If the robot was following a wall but there is a new destination,
 		//this will force the robot to recalculate its distance from the destination
 		//so it does not get stuck on a wall
-		boolean destinationIsNew = !this.destination.equals(destination);
-		if (destinationIsNew)
+		if (this.destination != null)
 		{
-			this.followingWall = false;
-		}
-		
-		if (this.followingWall)
-		{
-			Direction checkDirection = this.currentDirection.rotateRight();
-			if (rc.canMove(checkDirection))
+			boolean destinationIsNew = !this.destination.equals(destination);
+			if (destinationIsNew)
 			{
-				rc.move(checkDirection);
+				this.followingWall = false;
 			}
 		}
+		
+		this.destination = destination;
+		
+		if (!this.followingWall)
+		{
+			MapLocation currentLocation = rc.getLocation();
+			Direction direction = currentLocation.directionTo(destination);
+			
+			if (rc.canMove(direction))
+			{
+				rc.move(direction);
+				this.currentDirection = direction;
+				return;
+			}
+
+			//--We will now follow the wall, and we need a distance to
+			//know when we can stop following the wall.
+			this.currentDirection = getNavigableDirection(rc, direction);
+			this.initialDistance = currentLocation.distanceSquaredTo(destination);
+			this.followingWall = true;
+		}
+		
+		//--The robot is following the wall!
+		
+		//--It checks if it can go around a corner
+		Direction cornerDirection = this.currentDirection.rotateRight();
+		if (rc.canMove(cornerDirection))
+		{
+			rc.move(cornerDirection);
+			//--If I am now closer than I was when I started,
+			//then I am no longer following the wall.
+			int currentDistance = rc.getLocation().distanceSquaredTo(this.destination);
+			if (currentDistance < this.initialDistance)
+			{
+				this.followingWall = false;
+				return;
+			}
+		}
+		
+		this.currentDirection = getNavigableDirection(rc, this.currentDirection);
+		rc.move(this.currentDirection);
+	}
+	
+	private static Direction getNavigableDirection(
+			RobotController rc, Direction direction)
+	{
+		while (!rc.canMove(direction))
+		{
+			direction = direction.rotateLeft();
+		}
+		
+		return direction;
 	}
 }
--- Soldier.java
+++ Soldier.java
@@ -16,15 +16,9 @@ public class Soldier {
 	final static int SELF_DESTRUCT_WALK_STEPS = 2;
 	final static int INVALID_COORDINATE = -1;
 	
-	private static MapLocation previousDestination = 
-			new MapLocation(INVALID_COORDINATE, INVALID_COORDINATE);
-	
 	public static void run(RobotController rc)
 	{
-		int robotID = rc.getRobot().getID();
-		rc.setIndicatorString(2, "" + robotID);
-		
-		MovementInfo movementStatus = new MovementInfo(robotID);
+		MovementLogic navigation = new MovementLogic();
 		
 		while (true)
 		{
@@ -47,7 +41,7 @@ public class Soldier {
 					else 
 					{
 						MapLocation destination = Communication.getDestination(rc);
-						goToDestination(destination, rc, movementStatus);
+						navigation.moveToward(destination, rc);
 					}
 					
 					rc.yield();
@@ -58,91 +52,6 @@ public class Soldier {
 				e.printStackTrace();
 			}
 		}
-	}
-	
-	//--TODO: This method will cause the robot to go into an infinite loop
-	//if the distance to the destination from the corners of an obstacle are never
-	//shorter than the initial distance when the robot first hit the obstacle.
-	
-	//--TODO: A robot needs to leave behind 'waypoints' to indicate that the path
-	//has shortcuts. Other robots will follow these 'waypoints' if they exist
-	private static void goToDestination(
-			MapLocation destination, RobotController rc, MovementInfo movementStatus) 
-			throws GameActionException 
-	{
-		boolean destinationIsNew = !previousDestination.equals(destination);
-		previousDestination = destination;
-		
-		//--If the robot was following a wall but there is a new destination,
-		//this will force the robot to recalculate its distance from the destination
-		//so it does not get stuck on a wall
-		if (destinationIsNew)
-		{
-			movementStatus.followingWall = false;
-		}
-		
-		if (movementStatus.followingWall) {
-			followWall(destination, rc, movementStatus);
-			return;
-		}
-		
-		//--check boolean and set direction
-		MapLocation currentLocation = rc.getLocation();
-		movementStatus.currentDirection = currentLocation.directionTo(destination);
-		
-		//--move
-		if (rc.canMove(movementStatus.currentDirection))
-		{
-			rc.move(movementStatus.currentDirection);
-		}
-		else
-		{
-			movementStatus.followingWall = true;
-			movementStatus.distance = currentLocation.distanceSquaredTo(destination);
-			movementStatus.currentDirection = getNavigableDirection(rc, movementStatus);
-			rc.move(movementStatus.currentDirection);
-		}
-		
-		return;
-	}
-
-	private static void followWall(
-			MapLocation destination, RobotController rc, MovementInfo movementStatus) 
-			throws GameActionException {
-		
-		Direction checkDirection = getDirectionToCheck(movementStatus);
-		
-		if (rc.canMove(checkDirection))
-		{
-			movementStatus.currentDirection = checkDirection;
-			int currentDistance = rc.getLocation().distanceSquaredTo(destination);
-			if (currentDistance < movementStatus.distance) {
-				movementStatus.followingWall = false;
-				return;
-			}
-		}
-		
-		if (rc.canMove(movementStatus.currentDirection))
-		{
-			rc.move(movementStatus.currentDirection);
-		}
-		else
-		{
-			movementStatus.followingWall = true;
-			movementStatus.currentDirection = getNavigableDirection(rc, movementStatus);
-			rc.move(movementStatus.currentDirection);
-		}
-		
-		return;
-	}
-	
-	private static Direction getDirectionToCheck(MovementInfo movementStatus) {
-		if (movementStatus.turningRight)
-		{
-			return movementStatus.currentDirection.rotateLeft().rotateLeft();
-		}
-		
-		return movementStatus.currentDirection.rotateRight().rotateRight();
 	}
 	
 	private static void attackAnEnemy(RobotController rc, Robot[] nearbyEnemies)
@@ -176,24 +85,5 @@ public class Soldier {
 		}
 		
 		rc.selfDestruct();
-	}
-	
-	private static Direction getNavigableDirection(
-			RobotController rc, MovementInfo movementStatus)
-	{
-		Direction navigableDirection = movementStatus.currentDirection;
-		while (!rc.canMove(navigableDirection))
-		{
-			if (movementStatus.turningRight)
-			{
-				navigableDirection = navigableDirection.rotateRight();
-			}
-			else
-			{
-				navigableDirection = navigableDirection.rotateLeft();
-			}
-		}
-		
-		return navigableDirection;
 	}
 }
